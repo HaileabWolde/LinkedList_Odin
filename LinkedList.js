67aeeeb652533f32e898b805177524e7b42f1c5f
Haileab <womahaileab1428@gmail.com>
2025-08-13 15:21:04 +0300

    working on the necessary functions

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,52 @@
 import node from "./Node.js";
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+ 
+ append = (value) => {
+        const newNode = node(value, null);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    prepend = (value)=>{
+        const newNode = node(value, null);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+    size = ()=>{
+        let count = 0;
+        let current = this.head;
+        while (current !== null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+    head = () => {
+        return this.head;
+    }
+    tail = () => {
+        if (this.head === null) return null;
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        return current;
+    }
+}
+/*
  const LinkedList = (function (){
    let  head = null;
         const append = (value) => {
@@ -18,5 +66,5 @@ import node from "./Node.js";
             append,
             getHead
         }
-})();
+})();*/
 export default LinkedList;
--- Main.js
+++ Main.js
@@ -1,5 +1,8 @@
 import LinkedList from './LinkedList.js';
-
-LinkedList.append(5);
-LinkedList.append(10);
-console.log(LinkedList.getHead()); // Logs: { value: 5, next: null }
+const linkedLsit = new LinkedList();
+linkedLsit.append(5);
+linkedLsit.append("dog");
+linkedLsit.append("cat");
+linkedLsit.prepend(3);
+linkedLsit.size(); // Returns: 4
+console.log(linkedLsit.size()); // Logs: { value: 5, next: null }
